723e953d5d2f566f31bb8e44fa0a007e97bd8575
ShreyanshBanerjee <shreyansh.banerjee.x@gmail.com>
2026-01-04 22:15:32 -0500

    fixed graphing

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-%matplotlib inline
+%matplotlib inline 
 
 from qiskit import QuantumCircuit, transpile
 from qiskit_aer import AerSimulator
 from qiskit.visualization import plot_histogram
+import matplotlib.pyplot as plt
+
 
 n_qubits = 3
 shots = 1000
@@ -19,7 +21,8 @@ tqc = transpile(qc, simulator)
 job = simulator.run(tqc, shots=shots)
 result = job.result()
 
-counts = result.get_counts()
+counts = dict(sorted(result.get_counts().items()))
 print("Results:", counts)
 
-plot_histogram(counts).show()
+counts
+plt.bar(counts.keys(), counts.values(), 1, edgecolor='black', linewidth=1)
